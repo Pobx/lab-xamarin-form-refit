7f0ae5712c263121dbd8d3baff516908675a6844
11430279 <geidtiphong@gmail.com>
2023-01-30 21:47:21 +0700

    feat: create model for receive response from identity service

---
+++ RefitLabWithXamarinForm/RefitLabWithXamarinForm/Models/ResponseToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace RefitLabWithXamarinForm.Models
+{
+    public class ResponseToken
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+        public string refresh_token { get; set; }
+        public string scope { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public bool Result { get; set; }
+        public string ResultID { get; set; }
+        public string ResultValue { get; set; }
+        public string ErrorCode { get; set; }
+        public bool IsError { get; set; }
+        public bool IsUnsuccessPage { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
+
